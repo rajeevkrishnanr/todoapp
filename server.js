1898c1d85e40065e198683512e340c94eb7ca617
Rajeev Krishnan <rajeevkrishnan_r@yahoo.co.in>
2016-01-22 01:46:40 +0530

    Fix Monodb Conn

--- server.js
+++ server.js
@@ -9,11 +9,17 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 //Config
+mongoose.set('debug', true);
 
-//mongoose.connect('mongodb://mongo:mongo@apollo.modulusmongo.net:27017/jaZ2abyz');
-//mongoose.connect('mongodb://rjv:rjv@ds047075.mongolab.com:47075/todo_test_rjv');
-mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+mongoose.connect('mongodb://todo:todo@apollo.modulusmongo.net:27017/Evyna8xa');
+//mongoose.connect('mongodb://qwerty:qwerty@ds047075.mongolab.com:47075/todo_test_rjv');
+//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
 
+
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -84,3 +90,4 @@ app.get('*',function(req,res){
 //Listen
 app.listen(8080);
 console.log("Todo App Started on Port 8080")
+
